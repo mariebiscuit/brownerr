bcbe06f5637fe0e6f078c815c529adb7cd6d896a
blakeshao <90582347+blakeshao@users.noreply.github.com>
2023-04-28 01:21:37 -0400

    feat: finished talent card component

--- frontend/src/components/TalentCard.tsx
+++ frontend/src/components/TalentCard.tsx
@@ -3,6 +3,11 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Rating } from 'react-simple-star-rating'
+import {GiRoundStar} from "react-icons/gi"
+import {AiOutlineSmile, AiOutlineFrown, AiOutlineMeh} from "react-icons/ai"
+
+
 /**
  * Variables needed for the input box
  * it includes a handler map that contains REPLFunction and its call command
@@ -21,27 +26,40 @@ interface TalentCardProps {
 export default function TalentCard(props: TalentCardProps) {
 
   return (
-    <Card style={{ width: '25rem' }}>
-      <Card.Img variant="top" className="card-img" src={props.user.profilePicPath} />
+    <Card className="talent-card">
+      {(() => {
+          switch(props.user.availability) {
+            case "open": return <Button className="availability green"><AiOutlineSmile size={20} className="avail-icons"></AiOutlineSmile> Open to Work</Button> ;
+            case "moderate": return <Button className="availability orange"><AiOutlineMeh size={20} className="avail-icons"></AiOutlineMeh> Slightly Busy</Button> ;
+            case "busy": return <Button className="availability red"><AiOutlineFrown size={20} className="avail-icons"></AiOutlineFrown> Unavailable</Button> ;
+            default: return <Button className="availability red"><AiOutlineFrown size={20} className="avail-icons"></AiOutlineFrown> Unavailable</Button> 
+          }
+        })()}
+    
+      <Card.Img variant="top" className="talent-card-img" src={props.user.profilePicPath} />
       <Card.Body className="talent-card-body">
-        <Card.Title >{props.user.name}</Card.Title>
+        <Card.Title className="text-left talent-card-title" >{props.user.name}</Card.Title>
        
-        <Row>
-          <Col sm="4"> <Button className="card-tag">{props.user.categories[0]}</Button> </Col>
-          <Col sm="4"> <Button className="card-tag">{props.user.subcategories[0]}</Button> </Col>
-          <Col sm="4"> <Button className="card-tag">{props.user.subcategories[1]}</Button></Col>
+        <Row className="px-3">
+          <Col> <Button className="card-tag">{props.user.categories[0]}</Button> </Col>
+          <Col> <Button className="card-tag">{props.user.subcategories[0]}</Button> </Col>
+          <Col> <Button className="card-tag">{props.user.subcategories[1]}</Button></Col>
         </Row>
        
-        <Row>
-          <Col sm="4"> 
-            <div>
-              <p>Rate per Hour:</p>
-              <p>${props.user.rate}</p>
+        <Row className="second-row-talent-card">
+          <Col sm="5"> 
+            <div className="talent-num-div" >
+              <p className="text-left talent-num-title">Rate Per Hour:</p>
+              <p className="talent-card-title" style={{fontSize: "20px"}}>${props.user.rate}</p>
             </div> 
-          </Col>
-          
-          <Col sm="8">
-
+          </Col >
+            
+          <Col sm="7">
+            <div className="talent-num-div" >
+              <p className="text-left talent-num-title" >Star Rating:</p>
+              <Rating initialValue={props.user.rating} allowHover={false} fillColor= {"#FF7A00"} disableFillHover={true} fillIcon={<GiRoundStar size={32}/>} emptyIcon={<GiRoundStar size={32}/>} className="talent-card-stars"/>
+   
+            </div>
           </Col>
 
         </Row>
