75edecf8422b4c78a3f4361c3f647062adcef9f5
blakeshao <90582347+blakeshao@users.noreply.github.com>
2023-05-07 15:26:45 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -149,6 +149,7 @@ class Job(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     job = db.Column(db.Integer, db.ForeignKey('service.id'))
+    
 
 
 # Creating the database with above defined table(s)
