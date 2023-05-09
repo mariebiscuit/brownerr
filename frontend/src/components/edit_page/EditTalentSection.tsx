0d42293eb480151c1759da2f8734d118fd7e64fb
blakeshao <90582347+blakeshao@users.noreply.github.com>
2023-05-08 22:35:19 -0400

    Update EditTalentSection.tsx

--- frontend/src/components/edit_page/EditTalentSection.tsx
+++ frontend/src/components/edit_page/EditTalentSection.tsx
@@ -1,5 +1,5 @@
 import { EditableUser, User } from "../../Utilities";
-import { getDataUser } from "../../App";
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
